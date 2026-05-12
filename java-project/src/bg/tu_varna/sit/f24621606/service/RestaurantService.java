9f57567e5902299f972b985fb2729d2c2b342dbf
Dimo33 <Dimo3201@gmail.com>
2026-05-12 08:11:08 +0300

    Добавени методи за управление на поръчки в RestaurantService

--- java-project/src/bg/tu_varna/sit/f24621606/service/RestaurantService.java
+++ java-project/src/bg/tu_varna/sit/f24621606/service/RestaurantService.java
@@ -1,6 +1,7 @@
 package bg.tu_varna.sit.f24621606.service;
 
 import bg.tu_varna.sit.f24621606.enums.ItemCategory;
+import bg.tu_varna.sit.f24621606.enums.OrderStatus;
 import bg.tu_varna.sit.f24621606.model.MenuItem;
 import bg.tu_varna.sit.f24621606.model.Order;
 import bg.tu_varna.sit.f24621606.model.Table;
@@ -60,4 +61,129 @@ public class RestaurantService {
     public List<Order> getOrders() {
         return orders;
     }
+
+
+    public void openOrder(int tableNumber) {  //Отваря нова поръчка за дадена маса
+
+        Table foundTable = null;
+
+        for (Table table : tables) {
+            if (table.getNumber() == tableNumber) {
+                foundTable = table;
+                break;
+            }
+        }
+
+        if (foundTable == null) {
+            throw new IllegalArgumentException("Масата не съществува.");
+        }
+
+        if (foundTable.getStatus().name().equals("OCCUPIED")) {
+            throw new IllegalArgumentException("Масата е заета.");
+        }
+
+        Order order = new Order(nextOrderId, tableNumber);
+
+        orders.add(order);
+
+        foundTable.occupy();
+
+        nextOrderId++;
+    }
+
+    public void addItemToOrder(int orderId, int itemId, int quantity) {   // Намира поръчката,проверява дали е OPEN,намира артикула от менюто,намалява наличността, добавя артикула към поръчката
+
+        Order foundOrder = null;
+
+        for (Order order : orders) {
+            if (order.getId() == orderId) {
+                foundOrder = order;
+                break;
+            }
+        }
+
+        if (foundOrder == null) {
+            throw new IllegalArgumentException("Поръчката не съществува.");
+        }
+
+        if (foundOrder.getStatus() != OrderStatus.OPEN) {
+            throw new IllegalArgumentException("Поръчката не е отворена.");
+        }
+
+        MenuItem foundItem = null;
+
+        for (MenuItem item : menuItems) {
+            if (item.getId() == itemId) {
+                foundItem = item;
+                break;
+            }
+        }
+
+        if (foundItem == null) {
+            throw new IllegalArgumentException("Артикулът не съществува.");
+        }
+
+        foundItem.decreaseQuantity(quantity);
+
+        foundOrder.addItem(foundItem, quantity);
+    }
+
+    public void closeOrder(int orderId) { //намери поръчката, провери дали е OPEN, смени статуса на PAID, освободи масата
+
+        Order foundOrder = null;
+
+        for (Order order : orders) {
+            if (order.getId() == orderId) {
+                foundOrder = order;
+                break;
+            }
+        }
+
+        if (foundOrder == null) {
+            throw new IllegalArgumentException("Поръчката не съществува.");
+        }
+
+        if (foundOrder.getStatus() != OrderStatus.OPEN) {
+            throw new IllegalArgumentException("Поръчката вече е приключена.");
+        }
+
+        foundOrder.setStatus(OrderStatus.PAID);
+
+        for (Table table : tables) {
+            if (table.getNumber() == foundOrder.getTableNumber()) {
+                table.free();
+                break;
+            }
+        }
+    }
+
+    public void cancelOrder(int orderId) {  // намира поръчката, проверява дали е OPEN,   сменя статуса на отказана, освобождава масата
+
+        Order foundOrder = null;
+
+        for (Order order : orders) {
+            if (order.getId() == orderId) {
+                foundOrder = order;
+                break;
+            }
+        }
+
+        if (foundOrder == null) {
+            throw new IllegalArgumentException("Поръчката не съществува.");
+        }
+
+        if (foundOrder.getStatus() != OrderStatus.OPEN) {
+            throw new IllegalArgumentException("Поръчката не може да бъде отказана.");
+        }
+
+        foundOrder.setStatus(OrderStatus.CANCELED);
+
+        for (Table table : tables) {
+            if (table.getNumber() == foundOrder.getTableNumber()) {
+                table.free();
+                break;
+            }
+        }
+    }
+
 }
